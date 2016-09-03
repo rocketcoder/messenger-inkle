7ae391071b36f6c314f713e372064375c88e02a3
unknown <cjmkamcem4@hotmail.com>
2016-09-03 08:28:25 -0400

    trying to fix story session management

--- chatDialogs/testChatDialog.js
+++ chatDialogs/testChatDialog.js
@@ -1,3 +1,4 @@
+"use strict"
 var Story = require('inkjs').Story;
 var json = require('./testChatDialog.json');
 
@@ -5,10 +6,10 @@ var json = require('./testChatDialog.json');
 module.exports = function (storyState){
     var inkStory = new Story(json);
     if(storyState)
-        inkStory.LoadJson(JSON.stringify(storyState));
+        inkStory.LoadJson(storyState);
         
     if(inkStory.canContinue)
         return { "value" : inkStory.Continue(), "state" : inkStory.storyState }; 
     else
-        return { "value" : null, "state" : inkStory.storyState };    
+        return { "value" : null, "state" : inkStory.storyState.json };    
 }
