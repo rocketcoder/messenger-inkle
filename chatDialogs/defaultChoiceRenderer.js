718b8d59b76a89f60bf66924a60e7b0986f1e53f
unknown <cjmkamcem4@hotmail.com>
2016-09-03 18:41:04 -0400

    trying to fix story session management

--- chatDialogs/defaultChoiceRenderer.js
+++ chatDialogs/defaultChoiceRenderer.js
@@ -1,22 +1,48 @@
 "use strict"
 module.exports = function(senderId, messageText, choices){
-     let messageData = { 
-            recipient: {
-                id: senderId
-            },          
-            message: {
-                attachment: {
-                    type: "template",                   
-                    payload: {
-                        template_type: "generic",  
-                        item_url: "https://www.oculus.com/en-us/rift/",               
-                        image_url: "", 
-                        elements: []                     
-                    }
-                }
-            }
-     };
-         
+     var messageData = {
+    recipient: {
+      id: senderId
+    },
+    message: {
+      attachment: {
+        type: "template",
+        payload: {
+          template_type: "generic",
+          elements: [{
+            title: "rift",
+            subtitle: "Next-generation virtual reality",
+            item_url: "https://www.oculus.com/en-us/rift/",               
+            image_url: "https://fitessbot.azurewebsites.net" + "/assets/rift.png",
+            buttons: [{
+              type: "web_url",
+              url: "https://www.oculus.com/en-us/rift/",
+              title: "Open Web URL"
+            }, {
+              type: "postback",
+              title: "Call Postback",
+              payload: "1",
+            }],
+          }, {
+            title: "touch",
+            subtitle: "Your Hands, Now in VR",
+            item_url: "https://www.oculus.com/en-us/touch/",               
+            image_url: "https://fitessbot.azurewebsites.net" + "/assets/touch.png",
+            buttons: [{
+              type: "web_url",
+              url: "https://www.oculus.com/en-us/touch/",
+              title: "Open Web URL"
+            }, {
+              type: "postback",
+              title: "Call Postback",
+              payload: "2",
+            }]
+          }]
+        }
+      }
+    }
+  };  
+         /*
     for (let i = 0; i < choices.length; ++i) {
         let choice = choices[i];        
         messageData.message.attachment.payload.elements.push(
@@ -26,6 +52,7 @@ module.exports = function(senderId, messageText, choices){
             buttons : [{ type: "postback", title: choice.text, payload: i }]
         });
     }
+    */
     
     return JSON.stringify(messageData);   
 }
