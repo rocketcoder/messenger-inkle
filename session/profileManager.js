428ff42bb77d6d5de16f2ae856ecc3522fffc685
unknown <cjmkamcem4@hotmail.com>
2016-09-05 02:56:49 -0400

    refactor for test cases

--- session/profileManager.js
+++ session/profileManager.js
@@ -1,13 +1,14 @@
 "use strict"
 const request = require("request");
 
-function ProfileManager(PAGE_ACCESS_TOKEN){      
+function ProfileManager(PAGE_ACCESS_TOKEN){
+    this.PAGE_ACCESS_TOKEN = PAGE_ACCESS_TOKEN;      
     this.getProfile = (senderId) => {
         console.log("calling profile manager");
         let graphPromise = new Promise((resolve, reject) => {
             request({
                     uri: `https://graph.facebook.com/v2.6/${senderId}`,
-                    qs: { fields : "first_name,last_name,locale,timezone,gender", access_token: PAGE_ACCESS_TOKEN },
+                    qs: { fields : "first_name,last_name,locale,timezone,gender", access_token: this.PAGE_ACCESS_TOKEN },
                     method: 'GET'
                 }, function (error, response, body) {
                     if (!error && response.statusCode == 200) {
